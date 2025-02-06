d65db0a02a04717ad9fe7bde1f3acd0b80806205
Eli Goldner <egoldner@brandeis.edu>
2025-02-06 14:08:36 -0500

    type fixes

--- inference/interpretation.py
+++ inference/interpretation.py
@@ -1,54 +1,30 @@
-import pandas as pd
+import datetime
 import os
+from typing import Iterable
+
+import pandas as pd
+import pytz
 
 
-class AnaforaAnnotation:
-    def __init__(self, entities: list[AnaforaEntity]) -> None:
-        self.entities = entities
-
-    def __str__(self) -> str:
-        return "<data>\n" "</data>"
+def timestamp() -> str:
+    current_time = datetime.datetime.now(pytz.timezone("America/New_York"))
+    return current_time.strftime("%H:%M:%S  %m-%d-%Y")
 
 
 class AnaforaEntity:
-    def __init__(self, span: str, filename: str) -> None:
+    def __init__(self, span: tuple[int, int], filename: str) -> None:
         self.anafora_id = -1
         self.span = span
         self.filename = filename
+
+    def get_id_str(self) -> str:
+        return f"{self.anafora_id}@e@{self.filename}@llama"
 
     def set_id(self, anafora_id) -> None:
         self.anafora_id = anafora_id
 
 
-class Medication(AnaforaEntity):
-    def __init__(self, span: str, filename: str) -> None:
-        super().__init__(span, filename)
-        self.instructions: list[Instruction] = []
-        self.cuis: list[str] = []
-        self.tuis: list[str] = []
-
-    def __str__(self) -> str:
-        # to return the XML entry
-        instructions_str = "".join(
-            f"<instruction_condition>{instruction.get_id_str()}</instruction_condition>"
-            for instruction in self.instructions
-        )
-        return ""
-
-    def set_instructions(self, instructions: list[Instruction]) -> None:
-        self.instructions = instructions
-
-    def set_cuis(self, cui_str: str) -> None:
-        self.cuis = cui_str.split(",")
-
-    def set_tuis(self, tui_str: str) -> None:
-        self.tuis = tui_str.split(",")
-
-
 class Instruction(AnaforaEntity):
-    def get_id_str(self) -> str:
-        return f"{self.anafora_id}@e@{self.filename}@llama"
-
     def __str__(self) -> str:
         return (
             "<entity>\n"
@@ -59,6 +35,92 @@ class Instruction(AnaforaEntity):
             "<properties/>\n"
             "</entity>\n"
         )
+
+
+class Medication(AnaforaEntity):
+    def __init__(self, span: tuple[int, int], filename: str) -> None:
+        super().__init__(span, filename)
+        self.instructions: list[Instruction] = []
+        self.cui_str: str = ""
+        self.tui_str: str = ""
+
+    def __str__(self) -> str:
+        # to return the XML entry
+        instructions_str = "".join(
+            f"<instruction_condition>{instruction.get_id_str()}</instruction_condition>"
+            for instruction in self.instructions
+        )
+
+        properties_str = (
+            "<negation_indicator/>\n"
+            f"<associatedCode>{self.cui_str}</associatedCode>\n"
+            f"<associatedTuiCodes>{self.tui_str}</associatedCode>\n"
+            "<conditional/>\n"
+            "<generic/>\n"
+            "<subject/>\n"
+            "<uncertainty_indicator/>\n"
+            "<DocTimeRel/>\n"
+            "<historyOf/>\n"
+            "<allergy_indicator/>\n"
+            "<change_status_model/>\n"
+            "<dosage_model/>\n"
+            "<duration_model/>\n"
+            "<end_date/>\n"
+            "<form_model/>\n"
+            "<frequency_model/>\n"
+            "<route_model/>\n"
+        )
+        return (
+            "<entity>\n"
+            f"<id>{self.get_id_str()}</id>\n"
+            f"<span>{self.span[0],self.span[1]}</span>\n"
+            "<type>Medications/Drugs</type>\n"
+            "<parentsType>UMLSEntities</parentsType>\n"
+            "<properties>\n"
+            "</properties>"
+            "</entity>\n"
+        )
+        return ""
+
+    def set_instructions(self, instructions: Iterable[Instruction]) -> None:
+        self.instructions = list(instructions)
+
+    def set_cui_str(self, cui_str: str) -> None:
+        self.cui_str = cui_str
+
+    def set_tui_str(self, tui_str: str) -> None:
+        self.tui_str = tui_str
+
+
+class AnaforaAnnotation:
+    def __init__(self, entities: Iterable[AnaforaEntity]) -> None:
+        self.entities = AnaforaAnnotation.order_entities(entities)
+
+    def __str__(self) -> str:
+        entities_str = "".join(str(entity) for entity in self.entities)
+        return (
+            "<data>\n"
+            "<info>\n"
+            f"<savetime>{timestamp()}</savetime>\n"
+            "<progress>completed</progress>\n"
+            '<schema path="./" protocol="file">temporal.schema.xml</schema>'
+            "<annotations>\n"
+            f"{entities_str}"
+            "</annotations>"
+            "</info>"
+            "</data>"
+        )
+
+    @staticmethod
+    def order_entities(entities: Iterable[AnaforaEntity]) -> list[AnaforaEntity]:
+        def span(anafora_entity: AnaforaEntity) -> tuple[int, int]:
+            return anafora_entity.span
+
+        ordered_entities = sorted(entities, key=span, reverse=True)
+        # Anafora XML starts from 1 not 0
+        for anafora_id, anafora_entity in enumerate(ordered_entities, start=1):
+            anafora_entity.set_id(anafora_id)
+        return ordered_entities
 
 
 def to_anafora_files(corpus_frame: pd.DataFrame, output_dir: str) -> None:
