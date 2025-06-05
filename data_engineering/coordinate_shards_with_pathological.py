d3c34f62eae841386c696eb206096987481464c1
Eli Goldner <etgld@posteo.us>
2025-06-05 13:15:17 -0400

    Time to run

--- data_engineering/coordinate_shards_with_pathological.py
+++ data_engineering/coordinate_shards_with_pathological.py
@@ -17,33 +17,8 @@ parser.add_argument("--output_dir", type=str)
 def __section_hash(
     base_filename: str,
     section_identifier: str,
-    # section_begin: int, section_end: int
 ) -> str:
     return f"{base_filename.strip()} - {section_identifier.strip()}"
-    # - {section_begin},{section_end}"
-
-
-def __tsv_to_section_hashes(target_tsv: str) -> set[str]:
-    # assumes format of the 'preimage' frames
-    # def __local_section_hash(row: pd.Series) -> str:
-    #     section_begin, section_end = [
-    #         int(i.strip()) for i in row.section_offsets.split(",")
-    #     ]
-    #     return __section_hash(
-    #         base_filename=row.filename,
-    #         section_identifier=row.section_identifier,
-    #         section_begin=section_begin,
-    #         section_end=section_end,
-    #     )
-
-    df = pd.read_csv(target_tsv, sep="\t")
-    return set(
-        df.apply(
-            __section_hash,
-            # __local_section_hash,
-            axis=1,
-        ).to_list()
-    )
 
 
 def __get_relevant_paths(input_folder: str) -> Iterable[str]:
@@ -55,39 +30,20 @@ def __get_relevant_paths(input_folder: str) -> Iterable[str]:
 
 
 def __build_frame(
-    shard_bundle_dir: list[str],
-    # empty_meds_preimage_tsv: str,
+    shard_bundle_dirs: list[str],
 ) -> pd.DataFrame:
-    gc.enable()
-    gc.collect()
-
-    def __is_not_problem_section(row: pd.Series) -> bool:
-        return (
-            __section_hash(
-                base_filename=row.filename, section_identifier=row.section_identifier
-            )
-            not in problem_sections
-        )
-
-    # def get_id_number(fn: str) -> int:
-    #     return int(fn.split("_")[-1])
-
     def load_frame(frame_fn: str) -> pd.DataFrame:
         return pd.read_csv(frame_fn, sep="\t")
 
     full_frame = pd.concat(
         chain.from_iterable(
             map(load_frame, __get_relevant_paths(input_folder=shard_dir))
-            for shard_dir in shard_bundle_dir
+            for shard_dir in shard_bundle_dirs
             if "shard" in shard_dir
         )
     )
     gc.collect()
-    return full_frame.loc[full_frame.apply(__is_not_problem_section, axis=1)]
-    # filtered_frame = full_frame.loc[full_frame.apply(__is_not_problem_section, axis=1)]
-    # filtered_frame["int_study_id"] = filtered_frame["filename"].map(get_id_number)
-    # filtered_frame = filtered_frame.sort_values(by="int_study_id")
-    # return filtered_frame
+    return full_frame
 
 
 def __process(
@@ -98,23 +54,56 @@ def __process(
 ) -> None:
     gc.enable()
 
-    def get_id_number(fn: str) -> int:
-        return int(fn.split("_")[-1])
-
     raw_remainder_frame = __build_frame(
         [
             os.path.join(remainder_shards_bundled, dirname)
             for dirname in os.listdir(remainder_shards_bundled)
             if "agent_2" in dirname
-        ],
-        # empty_meds_preimage_tsv,
+        ]
     )
 
-    problem_sections = __tsv_to_section_hashes(empty_meds_preimage_tsv)
+    def __local_section_hash(row: pd.Series) -> str:
+        return __section_hash(
+            base_filename=row.filename, section_identifier=row.section_identifier
+        )
+
+    problem_sections = set(
+        pd.read_csv(empty_meds_preimage_tsv, sep="\t")
+        .apply(__local_section_hash, axis=1)
+        .to_list()
+    )
     fixed_shard_frame = __build_frame(
         [
             shard_folder_with_fixed,
         ]
+    )
+    sections_with_fixed = set(
+        fixed_shard_frame.apply(__local_section_hash, axis=1).to_list()
+    )
+
+    assert problem_sections.issubset(sections_with_fixed)
+
+    def get_id_number(fn: str) -> int:
+        return int(fn.split("_")[-1])
+
+    def __is_not_problem_section(row: pd.Series) -> bool:
+        return (
+            __section_hash(
+                base_filename=row.filename, section_identifier=row.section_identifier
+            )
+            not in problem_sections
+        )
+
+    filtered_remainder_frame = raw_remainder_frame.loc[
+        raw_remainder_frame.apply(__is_not_problem_section, axis=1)
+    ]
+    full_frame = pd.concat((filtered_remainder_frame, fixed_shard_frame))
+    gc.collect()
+    full_frame["int_study_id"] = full_frame["filename"].map(get_id_number)
+    full_frame = full_frame.sort_values(by="int_study_id")
+    full_frame.drop(columns=["int_study_id"], inplace=True)
+    full_frame.to_csv(
+        os.path.join(output_dir, "all_shards_merged.tsv"), sep="\t", index=False
     )
 
 
