806472bf6d8d3230dcbf18da73e1a049b7dd9e58
Eli Goldner <egoldner@brandeis.edu>
2025-06-10 17:57:26 -0400

    Reassembly for pathological cases working

--- data_engineering/coordinate_shards_with_pathological.py
+++ data_engineering/coordinate_shards_with_pathological.py
@@ -9,16 +9,18 @@ import pandas as pd
 
 parser = argparse.ArgumentParser(description="")
 parser.add_argument("--shard_folder_with_fixed", type=str)
-parser.add_argument("--remainder_shards_bundled", type=str)
-parser.add_argument("--empty_meds_preimage_tsv", type=str)
+parser.add_argument("--remainder_shards_bundled", type=str, default=None)
+parser.add_argument("--empty_meds_tsv", type=str, default=None)
 parser.add_argument("--output_dir", type=str)
 
+def __get_id_number(fn: str) -> int:
+    return int(fn.split("_")[-1])
 
 def __section_hash(
     base_filename: str,
     section_identifier: str,
 ) -> str:
-    return f"{base_filename.strip()} - {section_identifier.strip()}"
+    return f"{base_filename.strip().lower()} - {section_identifier.strip().lower()}"
 
 
 def __get_relevant_paths(input_folder: str) -> Iterable[str]:
@@ -32,13 +34,22 @@ def __get_relevant_paths(input_folder: str) -> Iterable[str]:
 def __build_frame(
     shard_bundle_dirs: list[str],
 ) -> pd.DataFrame:
+    if len(shard_bundle_dirs) == 0:
+        return pd.DataFrame([])
+
     def load_frame(frame_fn: str) -> pd.DataFrame:
         return pd.read_csv(frame_fn, sep="\t")
 
     full_frame = pd.concat(
         chain.from_iterable(
-            map(load_frame, __get_relevant_paths(input_folder=shard_dir))
-            for shard_dir in shard_bundle_dirs
+            map(
+                load_frame,
+                __get_relevant_paths(
+                    input_folder=os.path.join(shard_bundle_dir, shard_dir)
+                ),
+            )
+            for shard_bundle_dir in shard_bundle_dirs
+            for shard_dir in os.listdir(shard_bundle_dir)
             if "shard" in shard_dir
         )
     )
@@ -48,8 +59,8 @@ def __build_frame(
 
 def __process(
     shard_folder_with_fixed: str,
-    remainder_shards_bundled: str,
-    empty_meds_preimage_tsv: str,
+    remainder_shards_bundled: str | None,
+    empty_meds_tsv: str | None,
     output_dir: str,
 ) -> None:
     gc.enable()
@@ -57,7 +68,11 @@ def __process(
     original_shards_with_pathological = __build_frame(
         [
             os.path.join(remainder_shards_bundled, dirname)
-            for dirname in os.listdir(remainder_shards_bundled)
+            for dirname in (
+                os.listdir(remainder_shards_bundled)
+                if remainder_shards_bundled is not None
+                else []
+            )
             if "agent_2" in dirname
         ]
     )
@@ -67,45 +82,48 @@ def __process(
             base_filename=row.filename, section_identifier=row.section_identifier
         )
 
-    problem_sections = set(
-        pd.read_csv(empty_meds_preimage_tsv, sep="\t")
-        .apply(__local_section_hash, axis=1)
-        .to_list()
-    )
     fixed_shard_frame = __build_frame(
         [
             shard_folder_with_fixed,
         ]
     )
-    problem_sections_in_fixed_frames = set(
-        fixed_shard_frame.apply(__local_section_hash, axis=1).to_list()
-    )
-
-    problem_sections_in_original_frames = set(
-        original_shards_with_pathological.apply(__local_section_hash, axis=1).to_list()
-    )
-    assert problem_sections.issubset(problem_sections_in_fixed_frames)
-    assert problem_sections.issubset(problem_sections_in_original_frames)
-
-    def __get_id_number(fn: str) -> int:
-        return int(fn.split("_")[-1])
-
-    def __is_not_problem_section(row: pd.Series) -> bool:
-        return (
-            __section_hash(
-                base_filename=row.filename, section_identifier=row.section_identifier
-            )
-            not in problem_sections
+    if empty_meds_tsv is not None:
+        problem_sections = set(
+            pd.read_csv(empty_meds_tsv, sep="\t")
+            .apply(__local_section_hash, axis=1)
+            .to_list()
+        )
+        fixed_frames_sections = set(
+            fixed_shard_frame.apply(__local_section_hash, axis=1).to_list()
         )
 
-    # filtered_remainder_frame = original_shards_with_pathological.loc[
-    #     original_shards_with_pathological.apply(__is_not_problem_section, axis=1)
-    # ]
-    # full_frame = pd.concat((filtered_remainder_frame, fixed_shard_frame))
-    filtered_fixed_frame = fixed_shard_frame.loc[
-        fixed_shard_frame.apply(__is_not_problem_section, axis=1)
-    ]
-    full_frame = pd.concat((original_shards_with_pathological, filtered_fixed_frame))
+        # original_frames_sections = (
+        #     set(
+        #         original_shards_with_pathological.apply(
+        #             __local_section_hash, axis=1
+        #         ).to_list()
+        #     )
+        #     if len(original_shards_with_pathological) > 0
+        #     else set()
+        # )
+
+        # assert problem_sections.issubset(
+        #     fixed_frames_sections
+        # ), problem_sections.intersection(fixed_frames_sections)
+        # assert problem_sections.issubset(original_frames_sections)
+
+
+        def __is_not_problem_section(row: pd.Series) -> bool:
+            return __local_section_hash(row) not in problem_sections
+
+        # filtered_remainder_frame = original_shards_with_pathological.loc[
+        #     original_shards_with_pathological.apply(__is_not_problem_section, axis=1)
+        # ]
+        # full_frame = pd.concat((filtered_remainder_frame, fixed_shard_frame))
+        fixed_shard_frame = fixed_shard_frame.loc[
+            fixed_shard_frame.apply(__is_not_problem_section, axis=1)
+        ]
+    full_frame = pd.concat((original_shards_with_pathological, fixed_shard_frame))
     gc.collect()
     full_frame["int_study_id"] = full_frame["filename"].map(__get_id_number)
     full_frame = full_frame.sort_values(by="int_study_id")
@@ -120,7 +138,7 @@ def main() -> None:
     __process(
         args.shard_folder_with_fixed,
         args.remainder_shards_bundled,
-        args.empty_meds_preimage_tsv,
+        args.empty_meds_tsv,
         args.output_dir,
     )
 
