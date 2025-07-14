0b8a05c22f8121e52804bbf891d07fcda5a25a4b
Eli Goldner <etgld@posteo.us>
2025-07-14 12:33:09 -0400

    Minor fix

--- src/crico/data_engineering/format_anafora_xml_directory.py
+++ src/crico/data_engineering/format_anafora_xml_directory.py
@@ -2,6 +2,7 @@ import argparse
 import logging
 import os
 import shutil
+from functools import partial
 
 from ..utils import basename_no_ext, mkdir
 
@@ -18,6 +19,17 @@ logging.basicConfig(
 )
 
 
+def make_new_directory_and_copy_file(
+    input_folder: str, output_folder: str, fn: str
+) -> None:
+    anafora_identifier = basename_no_ext(fn)
+    identifier_dir = os.path.join(output_folder, anafora_identifier)
+    mkdir(identifier_dir)
+    source_file_path = os.path.join(input_folder, fn)
+    dest_file_path = os.path.join(identifier_dir, fn)
+    shutil.copyfile(source_file_path, dest_file_path)
+
+
 def reshape(
     input_folder: str,
     output_folder: str,
@@ -25,16 +37,11 @@ def reshape(
     def is_xml(fn: str) -> bool:
         return fn.lower().strip().endswith("xml")
 
-    def make_new_directory_and_copy_file(fn: str) -> None:
-        anafora_identifier = basename_no_ext(fn)
-        identifier_dir = os.path.join(output_folder, anafora_identifier)
-        mkdir(identifier_dir)
-        source_file_path = os.path.join(input_folder, fn)
-        dest_file_path = os.path.join(identifier_dir, fn)
-        shutil.copyfile(source_file_path, dest_file_path)
-
+    make_new_dir_then_copy = partial(
+        make_new_directory_and_copy_file, input_folder, output_folder
+    )
     for xml_fn in filter(is_xml, os.listdir(input_folder)):
-        make_new_directory_and_copy_file(xml_fn)
+        make_new_dir_then_copy(xml_fn)
 
 
 def main() -> None:
